3a4ff9a47234b6befc597c43b26f96d86389db96
dmanbuhnik <dmanbuhnik@45603b5f-b0bf-acf2-2072-de98a5d81337>
2010-03-26 22:25:19 +0000

    lower the minSDK to 3 changed the prefix to suffix

--- src/com/IsraelPack/IsraelPack.java
+++ src/com/IsraelPack/IsraelPack.java
@@ -126,7 +126,7 @@ public class IsraelPack extends Activity {
 		for (String fileItem : fileList) {
 			String sdFile = "/sdcard/IsraelPack/" + fileItem;
 			String[] commands = {
-					"cat " + sdFile + " > /system/fonts/test_" + fileItem,
+					"cat " + sdFile + " > /system/fonts/" + fileItem + "_test",
 					"chmod 644 /system/fonts/" + fileItem };
 			printStatus("-I- Replacing " + fileItem);
 			sendShell(commands, "Replace " + fileItem);
